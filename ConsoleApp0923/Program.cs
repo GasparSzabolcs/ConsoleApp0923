6a38c831b0313a7ab488889ea1e57245cf45ec06
Szabolcs Gaspar <396kij@gmail.com>
2020-09-23 16:07:56 +0200

    igen

--- ConsoleApp0923/Program.cs
+++ ConsoleApp0923/Program.cs
@@ -10,6 +10,38 @@ namespace ConsoleApp0923
     {
         static void Main(string[] args)
         {
+            //fagy
+            Console.WriteLine("adja meg a kulso homersekletet pls");
+            double fagy = Convert.ToDouble(Console.ReadLine());
+            if (fagy <= 0)
+            {
+                Console.WriteLine("fagy van odakint");
+            }
+            //paritas
+            Console.WriteLine("adj meg egy egesz szamot pls");
+            int szam = Convert.ToInt32(Console.ReadLine());
+            if (szam % 2 == 0)
+            {
+                Console.WriteLine("paros a szam");
+            }
+
+            //oszthato3
+            Console.WriteLine("adj meg egy szamot");
+            int szam2 = Convert.ToInt32(Console.ReadLine());
+            if (szam2 % 3 ==0)
+            {
+                Console.WriteLine("oszthato 3al");
+            }
+            //kozotte
+            Console.WriteLine("adjon meg egy szamot");
+            int kozotte = Convert.ToInt32(Console.ReadLine());
+            if (kozotte >= -30 && kozotte <= 40)
+            {
+                Console.WriteLine("-30 és 40 kozot van");
+            }
+
+
+            Console.ReadKey();
         }
     }
 }
